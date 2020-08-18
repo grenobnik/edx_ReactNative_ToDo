535f24e65bcf55eca3532307529896db6c4154f8
Kirill Vankov <grenobnik@gmail.com>
2020-08-18 16:29:56 +0200

    fix export class

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import "./styles.css";
 
-class App extends React.Component {
+export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
