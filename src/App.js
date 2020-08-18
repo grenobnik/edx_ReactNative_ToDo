04e746b130ebef72d2830f87aa7113213d6a241e
Kirill Vankov <grenobnik@gmail.com>
2020-08-18 17:23:46 +0200

    working checkbox

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ let id = 0;
 
 const Todo = (props) => (
   <li>
-    <input type="checkbox" />
+    <input
+      type="checkbox"
+      checked={props.todo.checked}
+      onChange={props.onToggle}
+    />
     <button onClick={props.onDelete}>delete</button>
     <span>{props.todo.text}</span>
   </li>
@@ -23,7 +27,16 @@ export default class App extends React.Component {
   addTodo() {
     const text = prompt("TODO text goes here");
     this.setState({
-      todos: [...this.state.todos, { id: id++, text: text }]
+      todos: [...this.state.todos, { id: id++, text: text, checked: false }]
+    });
+  }
+
+  toggleTodoCheckbox(id) {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id !== id) return todo;
+        return { ...todo, checked: !todo.checked };
+      })
     });
   }
 
@@ -40,7 +53,11 @@ export default class App extends React.Component {
         <button onClick={() => this.addTodo()}>Add TODO</button>
         <ul>
           {this.state.todos.map((todo) => (
-            <Todo onDelete={() => this.removeTodo(todo.id)} todo={todo} />
+            <Todo
+              onToggle={() => this.toggleTodoCheckbox(todo.id)}
+              onDelete={() => this.removeTodo(todo.id)}
+              todo={todo}
+            />
           ))}
         </ul>
       </div>
