d2229c1a88f000538f511ae34a08fab9b5cceec6
Kirill Vankov <grenobnik@gmail.com>
2020-08-18 17:14:27 +0200

    delete TODO works

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { render } from "react-dom";
 import "./styles.css";
 
+let id = 0;
+
 const Todo = (props) => (
   <li>
     <input type="checkbox" />
-    <button>delete</button>
+    <button onClick={props.onDelete}>delete</button>
     <span>{props.todo.text}</span>
   </li>
 );
@@ -21,7 +23,13 @@ export default class App extends React.Component {
   addTodo() {
     const text = prompt("TODO text goes here");
     this.setState({
-      todos: [...this.state.todos, { text: text }]
+      todos: [...this.state.todos, { id: id++, text: text }]
+    });
+  }
+
+  removeTodo(id) {
+    this.setState({
+      todos: this.state.todos.filter((todo) => todo.id !== id)
     });
   }
 
@@ -32,7 +40,7 @@ export default class App extends React.Component {
         <button onClick={() => this.addTodo()}>Add TODO</button>
         <ul>
           {this.state.todos.map((todo) => (
-            <Todo todo={todo} />
+            <Todo onDelete={() => this.removeTodo(todo.id)} todo={todo} />
           ))}
         </ul>
       </div>
