6fbb77913ff9585a2ce1ec5e4064cc6815994661
Kirill Vankov <grenobnik@gmail.com>
2020-08-18 16:17:50 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { render } from "react-dom";
+import "./styles.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: []
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1> TODO App </h1>
+        <ul></ul>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
